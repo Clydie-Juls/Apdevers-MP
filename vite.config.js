deedf90710c59d5799daea73d2a413f4ae1a2cdf
lenzrivera <lenz_gio_rivera@dlsu.edu.ph>
2024-04-05 18:04:56 +0800

    Test

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
       "/api": {
         target: "https://www.apdevers-mp.onrender.com",
         changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
       },
     },
   },
